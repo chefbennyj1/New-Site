0df26e2d395697a1680c458c1f010506ac28e3de
Benjamin Anderson <chefbennyj@gmail.com>
2025-10-29 17:34:23 -0400

    asset sorting

--- views/partials/volume-1/pages/page2/page2.js
+++ views/partials/volume-1/pages/page2/page2.js
@@ -1,10 +1,39 @@
 export function init(container) {
-     
+    let videos = container.querySelectorAll('video[data-src]');
     container.addEventListener('view_visible',() => {        
-
+       enableMedia(videos);
     })
     container.addEventListener('view_hidden',() => {        
-        
+        videos.forEach(video => {
+            video.pause();           
+            video.currentTime = 0;
+            console.log(`pausing video: ${video.src}`)
+        });
     });
     console.log(`loading ${container.id}`)
  }
+
+ function enableMedia(videos) {
+    if (window.audioEnabled) {
+        window.transitionAudio.play();
+    }
+
+    videos.forEach(video => {
+        if (video.paused) {
+            video.src = video.dataset.src;
+            video.play().catch(err => console.log("Play error:", err));
+            console.log(`playing video: ${video.src}`)
+        }
+    })
+}
+
+document.addEventListener("visibilitychange", () => {
+  const videos = document.querySelectorAll('video');
+  videos.forEach(video => {
+    if (document.hidden) {
+      video.pause();
+    } else {
+      video.play();
+    }
+  });
+});
